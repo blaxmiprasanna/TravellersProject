ca910d06a15fb41c102a440b3da0874a934534bc
blaxmiprasanna <blaxmiprasanna@yahoo.com>
2022-05-26 11:15:56 -0400

    created travellers project

---
+++ src/main/java/com/TravellersProject/App.java
@@ -0,0 +1,13 @@
+package com.TravellersProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
